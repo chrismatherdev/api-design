75a6b3d52d52f209f8eacf7bcd6b16aa572c3d86
Chris Mather <chrismather21@outlook.com>
2023-09-10 22:28:54 +0100

    Initial commit

---
+++ src/modules/auth.ts
@@ -0,0 +1,45 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password) => {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
+};
+
+export const createJWT = (user) => {
+  const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
+  return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401);
+    res.json({ message: 'Not authorized!' });
+    return;
+  }
+
+  const [, token] = bearer.split(' ');
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: 'Not valid token!' });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.json({ message: 'Not valid token!' });
+    return;
+  }
+};
